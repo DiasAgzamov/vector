745080ab2ea3b1a77b509b8639c5fa7bc24b2707
DiasAgzamov <66073388+DiasAgzamov@users.noreply.github.com>
2020-08-25 20:28:42 +0600

    Update main.cpp

--- MiddleTemperature/main.cpp
+++ MiddleTemperature/main.cpp
@@ -4,22 +4,22 @@
 int main() {
     int N;
     std::cin >> N;
-    std::vector<int> t;
+    std::vector<int> temperature;
     for(int i = 0; i < N; ++i){
         int n;
         std::cin >> n;
-        t.push_back(n);
+        temperature.push_back(n);
     }
     int u = 0;
-    for(auto i : t) {
+    for(auto i : temperature) {
         u += i;
     }
     u = u / N;
 
     std::vector<int> K;
 
-    for (int x = 0; x < t.size(); ++x){
-        if(t[x] >= u){
+    for (int x = 0; x < temperature.size(); ++x){
+        if(temperature[x] >= u){
             K.push_back(x);
         }
     }
