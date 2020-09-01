7a2583f7e6b725dc26bbbcd9fb37e9beb5e9af66
DiasAgzamov <66073388+DiasAgzamov@users.noreply.github.com>
2020-09-01 13:10:29 +0600

    Update FactorialFib.cpp

--- FactorialFib/FactorialFib.cpp
+++ FactorialFib/FactorialFib.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 
 int Fibonacci(int n) {
-    if(n <= 1) return 1;
+    if (n <= 1) return 1;
     return Fibonacci(n - 2) + Fibonacci(n - 1);
 }
 
-int Factorial (int num){
+int Factorial(int num) {
     if (num <= 1) return 1;
-    return n * Factorial(num - 1)
+    return num * Factorial(num - 1);
 }
 
 int main() {
     int a;
     std::cin >> a;
-    std::cout << "Factorial is: " << std::endl
+    std::cout << "Factorial is: " << std::endl;
     std::cout << Factorial(a) << std::endl;
-    std::cout << "Fibonacci is: " << std::endl
+    std::cout << "Fibonacci is: " << std::endl;
     std::cout << Fibonacci(a) << std::endl;
     return 0;
 }
-
